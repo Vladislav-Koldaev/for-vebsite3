6f02991a6d9e3240f3fdb2e603eab6ec5cbc62fd
Владислав <koldaev18@mail.ru>
2023-02-02 22:39:40 +0300

    Вывод таблицы на главную

--- vebsite3/main/views.py
+++ vebsite3/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
-def index(requst):
-    return HttpResponse('<h4>Hello</h4>')
+def index(request):
+    task = Task.objects.all()
+    return render(request,"main/index.html",{'title':'Главная страница','tasks':'title'})
+
+def about(request):
+    return render(request,'main/about.html')
