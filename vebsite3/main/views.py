1ef7debffee9977d1b3951de3bd220148b9eb73f
Владислав <koldaev18@mail.ru>
2023-02-04 21:35:24 +0300

    Ошибка, урок 109

--- vebsite3/main/views.py
+++ vebsite3/main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 from .models  import Task
+from .forms import TaskForm
+
 
 def index(request):
     tasks = Task.objects.all()
@@ -8,3 +11,20 @@ def index(request):
 
 def about(request):
     return render(request,'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была не верной'
+
+    form = TaskForm()
+    context = {
+        "form":form,
+        "error":error
+    }
+    return render(request, "main/create.html")
